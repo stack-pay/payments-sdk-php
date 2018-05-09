85d0435f127d506fe3d79e8560612d283fee6275
Trip Somers <richard.somers@bluestarsports.com>
2018-05-09 16:15:39 -0500

    Add missing ApplicationName field for Merchant Link requests.

--- tests/functionals/Merchants/MerchantLinkTest.php
+++ tests/functionals/Merchants/MerchantLinkTest.php
@@ -38,40 +38,45 @@ final class MerchantLinkTest extends TestCase
                     (new structures\Rate())->setName('Rate A')
                 )
                 ->setExternalID('93hfon39f8')
+                ->setApplicationName('Test Application')
         );
 
-        $this->assertEquals('http://localhost:8080/merchant/application/40e334f62c19dcdddeba516d7049c130e4a66a46e73b35c89b08497df7e325de',  $merchant->link);
+        $this->assertEquals('http://localhost:8080/merchant/application/40e334f62c19dcdddeba516d7049c130e4a66a46e73b35c89b08497df7e325de', $merchant->link);
 
-        $this->assertCount(1,$curlProvider->calls);
+        $this->assertCount(1, $curlProvider->calls);
 
-        $this->assertEquals([
-        0 => [
-            'URL'  => 'https://api.mystackpay.com/api/merchants/link',
-            'Body' => [
-                'Body'   => [
-                    'ExternalId' => '93hfon39f8',
-                    'RateName'   => 'Rate A'
-                ],
-                'Header' => [
-                    'Application' => 'PaymentSystem',
-                    'ApiVersion'  => '1.0.0',
-                    'Mode'        => 'production',
-                    'Security'    => [
-                        'HashMethod' => 'SHA-256',
-                        'Hash'       => 'b4f2d2765516de58e7bdd699a39ec8271e35a387827b0395012c7866ba37214b'
+        $this->assertEquals(
+            [
+                0 => [
+                    'URL'  => 'https://api.mystackpay.com/api/merchants/link',
+                    'Body' => [
+                        'Body'   => [
+                            'ExternalId'        => '93hfon39f8',
+                            'RateName'          => 'Rate A',
+                            'ApplicationName'   => 'Test Application',
+                        ],
+                        'Header' => [
+                            'Application' => 'PaymentSystem',
+                            'ApiVersion'  => '1.0.0',
+                            'Mode'        => 'production',
+                            'Security'    => [
+                                'HashMethod' => 'SHA-256',
+                                'Hash'       => 'db39324c2ee45ee022de0cf18bb3790555e652e9309bc6e8d0d187a6b89259ef'
+                            ]
+                        ]
+                    ],
+                    'Headers' => [
+                        0 => ['Key' => 'Application',   'Value' => 'PaymentSystem'],
+                        1 => ['Key' => 'ApiVersion',    'Value' => '1.0.0'],
+                        2 => ['Key' => 'Mode',          'Value' => 'production'],
+                        3 => ['Key' => 'HashMethod',    'Value' => 'SHA-256'],
+                        4 => ['Key' => 'Hash',          'Value' => 'db39324c2ee45ee022de0cf18bb3790555e652e9309bc6e8d0d187a6b89259ef'],
+                        5 => ['Key' => 'Authorization', 'Value' => 'Bearer 7b986b7a09affd0d7bcb13214f5856b40f444858d728e5457931c82eea3d233c'],
+                        6 => ['Key' => 'Content-Type',  'Value' => 'application/json']
                     ]
                 ]
             ],
-            'Headers' => [
-                0 => ['Key' => 'Application',   'Value' => 'PaymentSystem'],
-                1 => ['Key' => 'ApiVersion',    'Value' => '1.0.0'],
-                2 => ['Key' => 'Mode',          'Value' => 'production'],
-                3 => ['Key' => 'HashMethod',    'Value' => 'SHA-256'],
-                4 => ['Key' => 'Hash',          'Value' => 'b4f2d2765516de58e7bdd699a39ec8271e35a387827b0395012c7866ba37214b'],
-                5 => ['Key' => 'Authorization', 'Value' => 'Bearer 7b986b7a09affd0d7bcb13214f5856b40f444858d728e5457931c82eea3d233c'],
-                6 => ['Key' => 'Content-Type',  'Value' => 'application/json']
-            ]
-        ]
-        ], $curlProvider->calls );
+            $curlProvider->calls
+        );
     }
 }
